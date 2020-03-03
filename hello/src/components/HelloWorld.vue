bb9b5250220449fb02d2a4a8053998123cc2360a
Natanael Silva <natanso.s17@hotmail.com>
2020-03-03 11:41:29 -0300

    Inicio do ToDoList do Ebook

---
+++ hello/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1 :title="msg">{{msg}}</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+  export default {
+    data (){
+      return{
+        msg: 'Bem-Vindo a sua primeira app com Vue.js'
+      }
+    }
+  }
+</script>
